55764b0b8601a0d0a0febfc0e12d53cdb1acbb75
danie <danie@DESKTOP-J7BQR55>
2021-05-31 15:40:34 -0500

    Subir Proyecto

---
+++ src/main/java/com/dwa/foro/WebForo1Application.java
@@ -0,0 +1,13 @@
+package com.dwa.foro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebForo1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebForo1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/dwa/foro/WebForo1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dwa.foro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebForo1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
